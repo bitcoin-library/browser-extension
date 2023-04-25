33a455f870642e15c327857f7716cca81d76387e
@s.roertgen <sroertgen@gmail.com>
2023-04-25 07:16:33 +0200

    Initital commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Bitcoin-Library Browser WebExtension
+
+WebExtension to send page data to the [bitcoin library metadata editor](https://github.com/bitcoin-library/metadata-editor).
+
+## Instructions for installing
+
+```
+$ git clone https://github.com/bitcoin-library/browser-extension.git
+$ cd skohub-extension
+$ npm install
+```
+
+### Add to Chrome
+
+- The extension still has to be added to the Chrome Store
+- For now: You can load the unpacked extension from the repo: In `Extensions`, enable Developer mode, click `load unpacked` and open the directory `skohub-extension/src`.
+
+### Add to Firefox
+
+- The extension still has to be added to the Firefox Add-ons Store
+- For now: Go to `Add-ons` in `Tools for Add-ons` click `Debug Add-ons` and load a new temporary add-on selecting the manifest in `skohub-extension/src`. You can also directly launch Firefox with a temporary profile by running `npm run start:firefox`
+
+## Usage
+
+After installing the extension click the new extension icon on any webpage, this will open a new tab to SkoHub Editor with the extracted content of the page.
+
+## Credits
+
+Based on the [SkoHub Browser Extension](https://github.com/skohub-io/skohub-extension)
---
+++ src/page.js
@@ -0,0 +1,120 @@
+
+/* global chrome */
+var attach = async () => {
+  const EDITOR_URL = 'http://localhost:5173/'
+
+  const loadSavedOptions = new Promise((resolve, reject) => {
+    chrome.storage.local.get({ defaultSchema: null }, (options) => {
+      resolve(options)
+    })
+  })
+  const { defaultSchema } = await loadSavedOptions
+
+  const url = new URL(EDITOR_URL)
+  defaultSchema && url.searchParams.set('schema', defaultSchema)
+
+  const getMetaTag = (attribute, value) => {
+    return (document.querySelector(`meta[${attribute}="${value}"]`) &&
+      document.querySelector(`meta[${attribute}="${value}"]`).content) || null
+  }
+
+  const pageGetData = () => {
+    return {
+      name: getMetaTag('property', 'og:title') ||
+        getMetaTag('name', 'twitter:title') ||
+        document.title,
+      id: getMetaTag('property', 'og:url') ||
+        window.location.href,
+      description: getMetaTag('property', 'og:description') ||
+        getMetaTag('name', 'twitter:description') ||
+        getMetaTag('name', 'description'),
+      author: getMetaTag('name', 'author'),
+      image: getMetaTag('property', 'og:image') ||
+        getMetaTag('name', 'twitter:image'),
+      locale: getMetaTag('property', 'og:locale')
+    }
+  }
+
+  const data = pageGetData()
+  Object.entries(data)
+    .filter(([key, val]) => val !== null)
+    .forEach(([key, value]) => {
+      url.searchParams.set(key, encodeURIComponent(value))
+    })
+
+  const styleContent = `
+    #SkoHubFrame {
+      position: fixed;
+      height: 100%;
+      width: 500px;
+      top: 0;
+      right: 0;
+      z-index: 2147483647;
+      box-shadow: 0px 3px 15px hsla(0, 0%, 0%, 0.2);
+      transition: transform 0.5s;
+      border: none;
+    }
+
+    #SkoHubCollapseBtn {
+      position: fixed;
+      top: 0;
+      right: 500px;
+      width: 40px;
+      height: 40px;
+      background-size: contain;
+      background-image: url(${chrome.runtime.getURL('icons/chevron-right.svg')});
+      background-color: hsl(29, 99%, 52%);
+      z-index: 2147483647;
+      border: none;
+      border-radius: 0 0 0 14px;
+      transition: transform 0.5s;
+    }
+
+    #SkoHubCollapseBtn.collapsed {
+      transition: transform 0.5s;
+      transform: translateX(500px);
+      background-image: url(${chrome.runtime.getURL('icons/chevron-left.svg')});
+    }
+
+    #SkoHubFrame.collapsed {
+      transition: transform 0.5s;
+      transform: translateX(500px);
+    }
+  `
+
+  const SkoHubCollapseBtn = document.createElement('button')
+  SkoHubCollapseBtn.id = 'SkoHubCollapseBtn'
+  SkoHubCollapseBtn.title = 'Collapse'
+  SkoHubCollapseBtn.addEventListener('click', () => {
+    const iframe = document.getElementById('SkoHubFrame')
+    if (iframe.classList.contains('collapsed')) {
+      iframe.classList.remove('collapsed')
+      SkoHubCollapseBtn.classList.remove('collapsed')
+      SkoHubCollapseBtn.title = 'Collapse'
+    } else {
+      iframe.classList.add('collapsed')
+      SkoHubCollapseBtn.classList.add('collapsed')
+      SkoHubCollapseBtn.title = 'Expand'
+    }
+  })
+
+  const style = document.createElement('style')
+  style.appendChild(document.createTextNode(styleContent))
+
+  const iframe = document.createElement('iframe')
+  iframe.src = url
+  iframe.id = 'SkoHubFrame'
+
+  document.body.appendChild(style)
+  document.body.appendChild(iframe)
+  document.body.appendChild(SkoHubCollapseBtn)
+}
+
+var iframeExist = document.getElementById('SkoHubFrame')
+
+if (!iframeExist) {
+  attach()
+} else {
+  iframeExist.remove()
+  document.getElementById('SkoHubCollapseBtn').remove()
+}
